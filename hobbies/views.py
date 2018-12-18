8f7fca68a2c922d03390f785788bd56e2f18b486
keciring <45945744+keciring@users.noreply.github.com>
2018-12-18 17:12:29 +0100

    Farame

--- hobbies/views.py
+++ hobbies/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Reservation
 
 # Create your views here.
-
 def index(request):
-    return HttpResponse("Hiya, Products here")
+    list = Reservation.objects.order_by('-reservationNumber')[:5]
+    context = {'list': list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, e_id):
+    e = get_object_or_404(Reservation, pk=e_id)
+    return render(request, 'polls/detail.html', {'e': e})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
