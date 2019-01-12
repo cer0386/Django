6d5ad5a988eb4b89f97d52747f7c9c5f13035846
Roman <cerny9roman@gmail.com>
2019-01-12 14:04:46 +0100

    Database rework

--- hobbies/admin.py
+++ hobbies/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Customer,Car,Employee,Reservation,Model,Position, ReservForm
+from .models import Customer,Car,Employee,Reservation,Model,Position
 
 # Register your models here.
 admin.site.register(Customer)
@@ -8,4 +8,3 @@ admin.site.register(Employee)
 admin.site.register(Reservation)
 admin.site.register(Model)
 admin.site.register(Position)
-admin.site.register(ReservForm)
--- hobbies/models.py
+++ hobbies/models.py
@@ -57,10 +57,10 @@ class Car(models.Model):
 
 class Reservation(models.Model):
     reservationNumber = models.IntegerField(primary_key=True)
-    #customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    #employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    customer = models.CharField(max_length=150)
-    employee = models.CharField(max_length=150)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    #customer = models.CharField(max_length=150)
+    #employee = models.CharField(max_length=150)
     cars = models.ManyToManyField(Car)
     pickupD = models.DateField('Pickup date', null=True)
     returnD = models.DateField('Return date', null=True)
